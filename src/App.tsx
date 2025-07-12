fd68d852d8ffe52936aaeec005d925af214f4076
Neeraj Kumhar <webassistantsnotes@gmail.com>
2025-07-12 19:29:05 +0530

    hii

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
+import ScrollToTop from './components/ScrollToTop';
 import Footer from './components/Footer';
 import Home from './pages/Home';
 import Products from './pages/Products';
@@ -15,6 +16,7 @@ function App() {
   return (
     <CartProvider>
       <Router>
+        <ScrollToTop />
         <div className="min-h-screen bg-gradient-to-br from-blue-50 to-orange-50">
           <Header />
           <main>
